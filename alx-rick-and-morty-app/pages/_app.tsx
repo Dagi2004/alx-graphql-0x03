65ddc5c0efa12010911388f576d878619b2d3b8f
Dagi <dagmawimilkias@gmail.com>
2025-02-01 22:01:15 +0300

    Implement Test Error

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -3,9 +3,11 @@ import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/graphql/apolloClient";
 import ErrorBoundary from "@/components/ErrorBoundary";
+import ErrorProneComponent from "@/components/ErrorProneComponent";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
+      <ErrorProneComponent />
 <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
